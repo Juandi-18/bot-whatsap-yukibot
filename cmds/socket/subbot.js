802b1318ba48bbcf4b6f45e8b0f75a409c3b867f
Juandi-18 <jdtemocheg@gmail.com>
2026-04-13 02:57:48 -0500

    Update subbot.js

--- cmds/socket/subbot.js
+++ cmds/socket/subbot.js
@@ -1,42 +1,26 @@
-import { startSubBot } from '../../core/subs.js';
-
-if (!global.subBotFlags) global.subBotFlags = {}
+import ws from 'ws';
 
 export default {
-    command: ['code', 'qr'],
-    category: 'socket',
-    run: async (client, m, args, usedPrefix, command) => {
-        try {
-            const sender = m.sender;
-            const isCode = command.toLowerCase().includes('code');
+  command: ['bots', 'sockets'],
+  category: 'socket',
+  run: async (client, m) => {
+    const mainBotJid = client.decodeJid(client.user.id);
+    const activeSubs = (global.conns || []).filter(sock => sock.user && sock.ws?.readyState === 1);
 
-            // Cooldown de 1 minuto
-            if (!global.db.data.users[sender]) global.db.data.users[sender] = {}
-            let user = global.db.data.users[sender];
-            if (new Date() - (user.Subs || 0) < 60000) {
-                return client.reply(m.chat, `《✧》 Espera un minuto antes de pedir otro código. ♡`, m);
-            }
+    const mentionedJid = [];
+    const subList = [];
 
-            let phone = sender.split('@')[0];
-            if (args && args[0]) {
-                phone = args[0].replace(/\D/g, '');
-            }
+    activeSubs.forEach((sock) => {
+      const jid = client.decodeJid(sock.user.id);
+      mentionedJid.push(jid);
+      subList.push(`- [SubBot] › @${jid.split('@')[0]}`);
+    });
 
-            global.subBotFlags[sender] = {
-                active: true,
-                chatId: m.chat
-            };
+    let message = `*「✿」Sockets Activos (${activeSubs.length + 1})*\n\n`;
+    message += `❖ Principal › @${mainBotJid.split('@')[0]}\n`;
+    message += `✿ Subs Online › *${activeSubs.length}*\n\n`;
+    if (subList.length) message += subList.join('\n');
 
-            // Mensaje de espera mínimo
-            await client.reply(m.chat, `⏳ Generando ${isCode ? 'código' : 'QR'} para @${phone}...`, m, { mentions: [phone + '@s.whatsapp.net'] });
-            
-            // Ejecución
-            await startSubBot(m, client, '', isCode, phone, m.chat, global.subBotFlags, true);
-            
-            user.Subs = new Date() * 1;
-            
-        } catch (e) {
-            console.error("ERROR EN SUBBOT CMD:", e);
-        }
-    }
+    return await client.sendMessage(m.chat, { text: message, mentions: [mainBotJid, ...mentionedJid] }, { quoted: m });
+  },
 };
